e86f077904f1fe2e5a1b2c9570523b804f3defde
jamie <jamiepreece@somersetdesign.co.uk>
2018-11-01 12:47:03 +0000

    plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
-// Declare
-// (variables, includes and sources)
+/*|
+| | [Declarations]
+| | variables, includes and sources
+|*/ 
 
-var gulp         = require('gulp'),
-    gutil        = require('gulp-util'),
-    concat       = require('gulp-concat'),
-    uglify       = require('gulp-uglify'),
-    sass         = require('gulp-sass'),
-    watch        = require('gulp-watch'),
-    sourcemaps   = require('gulp-sourcemaps'),
-    connect      = require('gulp-connect'),
-    livereload   = require('gulp-livereload'),
-    wait         = require('gulp-wait'),
-    SFTPDelay    = null,
-    offlineDev   = true,
-    reloadLive   = null,
+var gulp            = require('gulp'),
+    gutil           = require('gulp-util'),
+    concat          = require('gulp-concat'),
+    uglify          = require('gulp-uglify'),
+    sass            = require('gulp-sass'),
+    watch           = require('gulp-watch'),
+    sourcemaps      = require('gulp-sourcemaps'),
+    connect         = require('gulp-connect'),
+    livereload      = require('gulp-livereload'),
+    wait            = require('gulp-wait'),
+    plumber         = require('gulp-plumber'),
+    SFTPDelay       = null,
+    offlineDev      = true,
+    reloadLive      = null,
+    compileSingle   = true,
+
+    // Add file types to the array.
+    // You can add file types and/or specific paths
 
     input  = {
       'img': 'source/img/*',
@@ -34,12 +41,9 @@ else{
   reloadLive = 'reloadPage';
 }
 
-// Initiate -------------------------------------
-
-/* Run live browser refresh */
-gulp.task('idle', function() {
-    console.log('OK');
-});
+/*|
+| | [Functions / Tasks]
+|*/
 
 /* Run live browser refresh */
 gulp.task('connect', function() {
@@ -48,61 +52,71 @@ gulp.task('connect', function() {
     });
 });
 
-// Function Tasks -------------------------------
-
-// JS Task
-// (concat javascript files, minify if --type production)
+/*|
+| | [Compile JS]
+| | concat javascript files, minify if --type production.
+|*/
 gulp.task('build-js', function() {
     gulp.src(input.javascript)
         .pipe(sourcemaps.init())
         .pipe(concat('bundle.js'))
-        //only uglify if gulp is ran with '--type production'
         .pipe(uglify())
         .pipe(gulp.dest(output.javascript))
         .pipe(wait(SFTPDelay))
         .pipe(livereload());
 });
 
-//SCSS Task
-// (compile scss files)
-gulp.task('build-css', function() {
+/*|
+| | [Compile saved file]
+| | compiles only the file saved.
+|*/
+gulp.task('build-css-single', function() {
 
-
-    // Update 29th August, 2018 -
-    // [Sass Watch] Added. Compiles saved only stylesheet for faster reloading.
     return watch('source/scss/**/*.scss', { ignoreInitial: false })
+        .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
-        .on('error', swallowError)
         .pipe(gulp.dest(output.stylesheets))
         .pipe(connect.reload())
         .pipe(wait(SFTPDelay))
         .pipe(livereload(reloadLive));
-
-
-    // Older compiling code - 
-    // Will want to set this up as an optional function to compile all stylesheets
-
-    // gulp.src('source/scss/**/*.scss')
-    //     .pipe(sourcemaps.init())
-    //     .pipe(sass({outputStyle: 'compressed'}))
-    //     .on('error', swallowError)
-    //     .pipe(gulp.dest(output.stylesheets))
-    //     .pipe(connect.reload())
-    //     .pipe(wait(SFTPDelay))
-    //     .pipe(livereload(reloadLive));
-
 });
 
-//HTML Task
-// (watch HTML files for changes)
+/*|
+| | [Compile all]
+| | compiles all scss files for project init.
+|*/
+gulp.task('build-css-all', function() {
+
+    gulp.src('source/scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest(output.stylesheets))
+        .pipe(connect.reload())
+        .pipe(wait(SFTPDelay))
+        .pipe(livereload(reloadLive));
+});
+
+/*|
+| | [HTML Task]
+| | reload on saving html file
+|*/
 gulp.task('watch-html', function() {
     gulp.src(input.html)
         .pipe(wait(SFTPDelay))
         .pipe(livereload());
 });
 
-// ERROR Debug
+/*|
+| | [Custom function]
+| | test js in terminal and such.
+|*/
+gulp.task('idle', function() {
+    console.log('OK');
+});
+
+// ERROR Debug [Deprecated]
 // Console logs errors to terminal instead of crashing gulp
 function swallowError (error) {
   // If you want details of the error in the console
@@ -114,14 +128,21 @@ function swallowError (error) {
 // Execute ------------------------------
 
 // Watch declared files for changes and run custom function
-gulp.task('watch', function() {;﻿
+gulp.task('watch', function() {
 
     livereload.listen();
     gulp.watch(input.javascript, ['build-js']);
-    gulp.watch(input.sass, ['build-css']);
+
+    if (compileSingle === true) {
+        gulp.watch(input.sass, ['build-css-single']);
+    }
+    else{
+        gulp.watch(input.sass, ['build-css-all']);
+    }
+
     gulp.watch(input.html, ['watch-html']);
 
 });
 
 /* run the watch task when gulp is called without arguments */
-gulp.task('default', ['build-js', 'build-css', 'watch', 'connect']);
+gulp.task('default', ['build-js', 'build-css-all', 'watch', 'connect']);
